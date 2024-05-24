f9a7274745f31fde58bbdc14153eda654e766b8c
softjkeee <softjkeee@gmail.com>
2024-05-24 15:59:20 +0300

    Markdown Preview

---
+++ src/FieldDefinitions/MarkdownPreviewField.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SalesRender\Plugin\Components\Form\FieldDefinitions;
+
+class MarkdownPreviewField extends FieldDefinition
+{
+
+    private string $previewer;
+
+    public function __construct(string $title, ?string $description, string $previewer, $default = null, $context = null)
+    {
+        parent::__construct($title, $description, fn() => [], $default, $context);
+        $this->previewer = $previewer;
+    }
+
+    public function getPreviewer(): string
+    {
+        return $this->previewer;
+    }
+
+    public function getDefinition(): string
+    {
+        return 'markdownPreview';
+    }
+
+    public function jsonSerialize()
+    {
+        return array_merge(parent::jsonSerialize(), [
+            'previewer' => $this->getPreviewer(),
+        ]);
+    }
+}
